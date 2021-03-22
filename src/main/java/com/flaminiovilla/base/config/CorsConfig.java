c57cde6a0ea9f14ba323b14016f1f3c33dd93a04
flaminio <vifla01@gmail.com>
2021-03-22 09:54:28 +0100

    comment first

--- src/main/java/com/flaminiovilla/base/config/CorsConfig.java
+++ src/main/java/com/flaminiovilla/base/config/CorsConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
-
+/**
+ * Il Cross-Origin Resource Sharing (CORS) è un meccanismo che usa header HTTP addizionali
+ * per indicare che un dominio dispone dell'autorizzazione per accedere alle risorse selezionate
+ * **/
 @Configuration
 public class CorsConfig {
 
@@ -14,8 +17,8 @@ public class CorsConfig {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
       config.setAllowCredentials(true);
-      config.addAllowedOrigin("*"); // e.g. http://domain1.com
-      config.addAllowedHeader("*");
+      config.addAllowedOrigin("https://geris.flaminiovilla.it"); // e.g. http://domain1.com
+      config.addAllowedHeader("http://flaminiovilla.it");
       config.addAllowedMethod("*");
 
       source.registerCorsConfiguration("/api/**", config);
