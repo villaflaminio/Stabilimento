58bca303eaf47414b74c2df464a225eb70161acc
flaminio <vifla01@gmail.com>
2021-03-22 12:28:46 +0100

    securtyRest

--- src/main/java/com/flaminiovilla/base/security/rest/AuthenticationRestController.java
+++ src/main/java/com/flaminiovilla/base/security/rest/AuthenticationRestController.java
@@ -1,16 +1,11 @@
 package com.flaminiovilla.base.security.rest;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.flaminiovilla.base.security.jwt.JWTFilter;
-import com.flaminiovilla.base.security.jwt.TokenProvider;
-import com.flaminiovilla.base.security.rest.dto.LoginDto;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import com.flaminiovilla.base.security.helper.UserHelper;
+import com.flaminiovilla.base.security.model.User;
+import com.flaminiovilla.base.security.rest.dto.LoginDTO;
+import com.flaminiovilla.base.security.rest.dto.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,52 +21,17 @@ import javax.validation.Valid;
 @RequestMapping("/api")
 public class AuthenticationRestController {
 
-   private final TokenProvider tokenProvider;
-
-   private final AuthenticationManagerBuilder authenticationManagerBuilder;
-
-   public AuthenticationRestController(TokenProvider tokenProvider, AuthenticationManagerBuilder authenticationManagerBuilder) {
-      this.tokenProvider = tokenProvider;
-      this.authenticationManagerBuilder = authenticationManagerBuilder;
-   }
+   @Autowired
+   UserHelper userHelper;
 
    @PostMapping("/authenticate")
-   public ResponseEntity<JWTToken> authorize(@Valid @RequestBody LoginDto loginDto) {
-
-      UsernamePasswordAuthenticationToken authenticationToken =
-         new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
-
-      Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-      //SecurityContextHolder è una classe di supporto, che forniscono l'accesso al contesto di protezione
-      SecurityContextHolder.getContext().setAuthentication(authentication);
-
-      boolean rememberMe = (loginDto.isRememberMe() == null) ? false : loginDto.isRememberMe();
-      String jwt = tokenProvider.createToken(authentication, rememberMe);
-
-      HttpHeaders httpHeaders = new HttpHeaders();
-      httpHeaders.add(JWTFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
-
-      return new ResponseEntity<>(new JWTToken(jwt), httpHeaders, HttpStatus.OK);
+   public ResponseEntity<UserHelper.JWTToken> authorize(@Valid @RequestBody LoginDTO loginDto) {
+      return userHelper.authorize(loginDto);
    }
 
-   /**
-    * Object to return as body in JWT Authentication.
-    */
-   static class JWTToken {
-
-      private String idToken;
-
-      JWTToken(String idToken) {
-         this.idToken = idToken;
-      }
-
-      @JsonProperty("id_token")
-      String getIdToken() {
-         return idToken;
-      }
-
-      void setIdToken(String idToken) {
-         this.idToken = idToken;
-      }
+   @PostMapping("/register")
+   public User authorize(@Valid @RequestBody UserDTO userDTO) {
+      return userHelper.register(userDTO);
    }
+
 }
