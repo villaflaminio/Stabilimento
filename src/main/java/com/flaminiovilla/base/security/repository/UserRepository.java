aeb7f8bbf1cdc7f753d0f4ec4c744063a20c2e27
flaminio <vifla01@gmail.com>
2021-03-22 09:49:07 +0100

    Initial commit

---
+++ src/main/java/com/flaminiovilla/base/security/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.flaminiovilla.base.security.repository;
+
+import com.flaminiovilla.base.security.model.User;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+   @EntityGraph(attributePaths = "authorities")
+   Optional<User> findOneWithAuthoritiesByUsername(String username);
+
+   @EntityGraph(attributePaths = "authorities")
+   Optional<User> findOneWithAuthoritiesByEmailIgnoreCase(String email);
+
+}
